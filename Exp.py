b2b13679c88ac1726b4438ba18c60f9506360d3e
Thomas Ghile <tmghile05@gmail.com>
2024-03-20 09:32:25 -0400

    Heil Hitler

--- Exp.py
+++ Exp.py
@@ -1,1 +1,12 @@
+def drawtriangle():
+    for i in range(5,0,-1):
+        print("*"*i,end=" "*(6-i)+(i*"*"))
+        print()
+drawtriangle()
 
+print()
+print("* * * *     *")
+print("      *     *")
+print("* * * * * * * ")
+print("*     *")
+print("*     * * * *")
---
+++ TG.py
@@ -0,0 +1,6 @@
+tuple=("Ice Cream","Taco","Burritos","MilkShake","Cake","Hamburger","Hotdogs")
+array=["Jake","Sally","Rick","Morgan","Carol","Jason","Henry"]
+for i in range(0,len(array)):
+    array.insert(i,array.pop())
+
+print(array)
