68fe851e4fc1458a06db59007ae345fdcf9ef504
Thomas Ghile <tmghile05@gmail.com>
2024-03-22 09:16:45 -0400

    hello.py

--- Exp.py
+++ Exp.py
@@ -14,6 +14,12 @@ def backspace():
     entry.delete(len(entry.get())-1,END)
 
 window=Tk();
+x=IntVar()
+def check():
+    if(x.get()==1):
+        print("done!")
+    else:
+        print("undone!")
 window.geometry("50x50")
 icon=ImageTk.PhotoImage(Image.open("Lion.jpg"))
 window.title("Whats up?")
@@ -31,4 +37,7 @@ delete_button=Button(window,command=delete,text="delete",font=("Arial",20,"bold"
 delete_button.pack(side=RIGHT)
 backspace_button=Button(window,command=backspace,text="backspace",font=("Arial",20,"bold"),fg="blue",bg="white")
 backspace_button.pack(side=RIGHT)
+photo=ImageTk.PhotoImage(Image.open("Python_color.png"))
+check_button=Checkbutton(window,text="If Mogos is dumb!",variable=x,onvalue=1,offvalue=0,font=("Arial",20),fg="green",bg="red",activeforeground="green",activebackground="red",command=check,image=photo,compound="left")
+check_button.pack()
 window.mainloop()
